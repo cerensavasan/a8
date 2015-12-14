a9cf471cfccdd84a9ec1c315053534e05bc4ef63
cerensavasan <cerensavasan@gmail.com>
2015-12-13 21:36:23 -0500

    Cookie test

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var express = require('express')
   , fs = require("fs")
   , url = require("url")
   , path = require("path")
-  , studentsFile = require("./studentFile.js")
-  , announcementFile = require("./announcementFile.js")
-  , reportFile = require("./reportFile.js")
+  , postsFile = require("./post.js")
 
 //SETTING UP SERVER
 var app = express();
@@ -13,101 +11,7 @@ var port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname)));
 
 //DECLARING ARRAYS FOR TEMPLATES
-var students = [
-{
-  name: "Carl Jodan",
-  quizzes: ["80", "88","57"],
-  hw1: "40",
-  midterm: "89",
-  imageURL: "./cats/cat1.jpg",
-},
-{
-  name: "Xiaochen Ding",
-  quizzes: ["70", "90","100"],
-  hw1: "70",
-  midterm: "94",
-  imageURL: "./cats/cat2.jpg",
-},
-{
-  name: "Ceren Savasan",
-  quizzes: ["100", "100","100"],
-  hw1: "90",
-  midterm: "95",
-  imageURL: "./cats/cat3.jpg",
-}
-]
-
-var quiz1s = [];
-var quiz2s = [];
-var quiz3s= [];
-var hw1s= [];
-var midterms= [];
-
-//POPULATE QUIZ GRADES FOR REPORT
-students.forEach( function(student) {
- quiz1s.push(Number(student.quizzes[0]));
- quiz2s.push(Number(student.quizzes[1]));
- quiz3s.push(Number(student.quizzes[2]));
- hw1s.push(Number(student.hw1));
- midterms.push(Number(student.midterm));
-})
-
-function calculateAverage(a){
-  var total = 0;
-  var count = 0;
-  a.forEach( function(elem){
-    var intA = parseInt(elem);
-    total += intA;
-    count += 1;
-    console.log("total is:" + total);
-  })
-  var newAve = Math.round(total/count);
-  return newAve;
-}
-
-console.log("quiz1s contains:");
-quiz1s.forEach(function(p){
-console.log(typeof p);
-});
-
-console.log("quiz2s contains: \n");
-quiz2s.forEach(function(p){
-console.log(typeof p);
-});
-
-
-var q1_ave = calculateAverage(quiz1s).toString();
-var q2_ave = calculateAverage(quiz2s).toString();
-var q3_ave = calculateAverage(quiz3s).toString();
-var hw1_ave = calculateAverage(hw1s).toString();
-var mid_ave = calculateAverage(midterms).toString();
-
-
-var report = [
-{
-  name: "Quiz 1",
-  points: quiz1s,
-  date: "July 8th, 2015",
-  average: q1_ave,
-  missing: ["Billy Jones", "David Tennant"],
-},
-{
-  name: "Quiz 2",
-  points: quiz2s,
-  date: "July 8th, 2015",
-  average: q2_ave,
-  missing: ["Billy Jones", "David Tennant"],
-},
-{
-  name: "Quiz 3",
-  points: quiz3s,
-  date: "July 8th, 2015",
-  average: q3_ave.toString(),
-  missing: ["Billy Jones"]
-}
-]
-
-var announcements = [
+var posts = [
 {
   order: "1",
   body: "Final exam has been cancelled, instead we will only have project presentations on December 17th.",
@@ -143,25 +47,13 @@ app.get('/content', function(req, res) {
 
 // send big block of html via templates
 function sendContent(req, res) {
-
 var str = "";
-
-students.forEach( function(p) {
- str += studentsFile.Compiled1(p);
+str +=  "<h1>Posts:</h1>";
+//populate with posts
+posts.forEach( function(p) {
+ str += postsFile.Compiled(p);
 })
-
-str +=  "<h1>Breakdown of Grade Components:</h1>";
-
-report.forEach( function(p) {
- str += reportFile.Compiled2(p);
-})
-
-str +=  "<h1>Announcements:</h1>";
-
-announcements.forEach( function(p) {
- str += announcementFile.Compiled3(p);
-})
-
+//send giant string of html
 res.end( str );
 }
 
--- post.js
+++ post.js
@@ -1,10 +1,10 @@
 var _ = require("underscore");
 
-var compiled3 = "<div class='announce'>" +
+var compiled = "<div class='posts'>" +
   "<p class='message'><%= order %>. <%= body %></p>" +
   "<p class='timestamp'><b>Posted at: </b> <%= timestamp %></p>" +
-  "<p class='author'><b>Written by: </b> <%= author %> </p>" +
+  "<p class='author'><b>Posted by: </b> <%= author %> </p>" +
   "<br></br>" +
 "</div>";
 
-exports.Compiled3 = _.template(compiled3);
+exports.Compiled = _.template(compiled);
